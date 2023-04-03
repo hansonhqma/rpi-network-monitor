2bc5e1e1b524845a306f84580e33b36f3d522508
CocoTang03 <qinwentang@outlook.com>
2023-04-03 00:26:57 -0400

    add some library

---
+++ frontEnd/rim/src/common/components/layouts/DirectorypageContent/TableSection/__index.js
@@ -0,0 +1,1 @@
+export { default as TableSection } from "./TableSection.component.jsx"; 
--- frontEnd/rim/src/pages/index.js
+++ frontEnd/rim/src/pages/index.js
@@ -8,7 +8,7 @@ import WebpageWrapper from '@/WebpageWrapper'
 import { HeroSection } from '@/HeroSection'
 import { FeaturesSection } from "@/FeaturesSection"
 import { ConnectionResults } from "@/common/components/layouts/HomepageContent/ConnectionResults/__index"
-import { PopupT } from "@/common/components/layouts/PopupT/__index"
+//import { PopupT } from "@/common/components/layouts/PopupT/__index"
 //Insert <PopupT /> when fixed
 
 const inter = Inter({ subsets: ['latin'] })
@@ -17,9 +17,9 @@ export default function Home() {
     
     return (
       <WebpageWrapper>
+        <ConnectionResults />
         <HeroSection />
         <FeaturesSection />
-        <ConnectionResults />
       </WebpageWrapper>
     )
   
