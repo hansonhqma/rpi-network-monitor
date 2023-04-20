fcdab2e6b255034830985bd004dc2ea9427ff78d
CocoTang03 <qinwentang@outlook.com>
2023-04-20 11:33:23 -0400

    Able to show building in color layer && setminzoom

--- frontEnd/rim/src/common/components/layouts/MappageContent/MapSection/MapSection.component.jsx
+++ frontEnd/rim/src/common/components/layouts/MappageContent/MapSection/MapSection.component.jsx
@@ -15,16 +15,56 @@ function MapSection() {
     const map = new mapboxgl.Map({
       container: mapContainer.current,
       style: 'mapbox://styles/mapbox/streets-v9',
-      center: [-73.678802, 42.730171],
-      zoom: 15
+      center: [-73.67570101928914, 42.72935756119415],
+      zoom: 15,
+      bearing: 15
     });
+    map.setMinZoom(14);
 
     const popup = new mapboxgl.Popup().setHTML('<h3>Reykjavik Roasters</h3><p>A good coffee shop</p>');
 
-    const marker = new mapboxgl.Marker()
+    map.on('load', () => {
+      map.setPaintProperty('building', 'fill-color', 
+      [
+        'interpolate',
+        // Set the exponential rate of change to 0.5
+        ['exponential', 0.5],
+        ['zoom'],
+        // When zoom is 15, buildings will be beige.
+
+        // When zoom is 18 or higher, buildings will be yellow.
+        1, 
+        '#ffd700'
+      ],
+      /*{
+        "type": "FeatureCollection",
+        "filter": ["within", "geometry", 
+          ["literal", 
+            {"type": "Polygon",
+              "coordinates": [[
+                [-73.68605458857701,42.73195202975475],
+                [-73.68642538303402,42.730726388709],
+                [-73.68423769573951,42.73023612550941],
+                [-73.68486804631573,42.728166082609846],
+                [-73.67808250775755,42.726804174605235],
+                [-73.67333633871152,42.72664074363564],
+                [-73.67233519367824,42.73018165158143],
+                [-73.66732946851243,42.730426783880915],
+                [-73.66299119093583,42.735220292273084],
+                [-73.67051831840752,42.73636414178196],
+                [-73.6717048606688,42.73241504820879],
+                [-73.68293993270771,42.73380406841096],
+                [-73.68379275995801,42.73178861715493]
+              ]]
+            }
+          ]]
+      }*/);
+    });
+    
+    /*const marker = new mapboxgl.Marker()
       .setLngLat([-73.678802, 42.730171])
       .setPopup(popup)
-      .addTo(map);
+      .addTo(map);*/
 
     return () => map.remove();
   }, []);
